8b5248b1a52198f9a16a9c66dcfd82fa27982639
spss20 <sppro.20@gmail.com>
2023-04-03 14:56:14 +0530

    Mountain added

---
+++ Playground/app.js
@@ -0,0 +1,13 @@
+console.log("Script Started");
+
+class Name {
+    constructor(name) {
+        this.name = name;
+    }
+    print() {
+        console.log("Hello I AM " + this.name);
+    }
+}
+
+const surya = new Name("Surya");
+const krishna = new Name("Krishna");
